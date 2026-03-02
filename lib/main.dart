95df589fded31e54462df2462008b4ef6ab6faef
Akarshana06 <akarshana027@gmail.com>
2026-03-02 13:06:41 +0530

    tracker done

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,11 @@ import 'package:flutter/material.dart';
 import 'mainpage/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
-}
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sanitrax',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF6B7B6A)),
-      ),
-      home: const HomePage(),
-    );
-  }
 }
