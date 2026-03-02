0ee393181bb4d2bc23c571b2d21f9dbd18dc61a1
nandhini <nandhu1407@gmail.com>
2026-03-02 16:28:34 +0530

    Resolved merge conflict with firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'mainpage/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
